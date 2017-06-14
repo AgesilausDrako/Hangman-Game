a6b3af3e7af9825ae85efdf00f64dd62c88dbb93
AgesilausDrako <agesilaus@yahoo.com>
2017-06-14 15:22:58 -0400

    added reset to javascript picture to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 //Words for the game
 var wordBank = ["javascript", "python", "ruby", 
 				"coffeescript", "java"];
+
 //Stores the word chosen
 var word = "";
+
 //Stores the answer board
 var guessArr = [];
 var wins = 0;
@@ -10,7 +12,7 @@ var chances = 6;
 var answerArr = [];
 
 //Gamestart function
-function init() {
+function randomWord() {
 	//Computer's random guess
 	word = wordBank[Math.floor(Math.random() * wordBank.length)];
 	//Array for guess storage 
@@ -18,50 +20,54 @@ function init() {
 		answerArr[i] = "_";
 	}
 	document.querySelector("#word").innerHTML = answerArr.join(" ");
+	document.querySelector("#numChances").innerHTML = chances;
 }
-init();
+randomWord();
 
 
 //Guess function
-document.onkeyup = function (event){
+document.onkeyup = function (event) {
+
 	var userGuess = event.key;
+	
 	guessArr.push(userGuess);
+	
+	for (var j=0; j<word.length; j++) {
+		if (word[j] === userGuess) {
+			answerArr[j] = userGuess;
+		} 
+	} 
 
-	for (var i=0; i<word.length; i++){
-		if (word[i] === userGuess){
-			answerArr[i] = userGuess;
-		}
-	}
-
-	var remaining_letters = answerArr.length;
-        // recount the remaining letters
-    for (i = 0; i < answerArr.length; i++) {
-        if (answerArr[i] !== '_') {
-            remaining_letters -= 1;
-        }
-    }
-
-    if (remaining_letters == 0){
-
-    }
-
-    
     document.querySelector("#numChances").innerHTML = chances;
     document.querySelector("#word").innerHTML = answerArr.join(" ");
     document.querySelector("#alreadyGuessed").innerHTML = guessArr.join();
-    
-    /*Reset which changes wins, losses, and guesses to 0 after 
-	6 attempts and clears the array*/
-	if (chances <= 0 || remaining_letters === 0) {
-		wins=0;
-		chances=6;
-		guessArr=[];
-		answerArr=[];
-	location.reload();
-	}
+   
+   	var remainingLetters = answerArr.length;
+
+    for (k = 0; k < answerArr.length; k++) {
+        if (answerArr[k] !== '_') {
+            remainingLetters -= 1;
+        }
+    }
+
+   //Reset variables and obtain a new word by recalling the randomWord function
+   	if (remainingLetters === 0 || chances < 0) {
+   		wins+=1;
+   		chances=6;
+   		guessArr=[];
+   		answerArr=[];
+   		remainingLetters = 0;
+   		randomWord();
+   		document.querySelector("#numChances").innerHTML = chances;
+   		document.querySelector("#numWins").innerHTML = wins;
+    	document.querySelector("#alreadyGuessed").innerHTML = "";
+   	}
+
 }
     
-
+/* if (word.indexOf() = -1) {
+		chances-=1;
+*/
 
 
 
